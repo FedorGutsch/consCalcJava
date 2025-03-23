329463c7f813e5ccd8dbc4a14e670d3ae5bc481f
Guchinskij Fedor <gucinskijfedor@gmail.com>
2025-03-23 20:46:45 +0300

    Зациклил работу программы, теперь она работает до ввода в меню -1

--- Calc.java
+++ Calc.java
@@ -11,7 +11,7 @@ public class Calc {
         menu = new Menu();
     }
 
-    public void  showMenu(){
+    public void  showMenu() throws Exception{
         System.out.println("Выберите систему счисления ");
         this.setSystem(menu.showMenu());
     }
@@ -27,27 +27,32 @@ public class Calc {
         this.number2 = menu.getNumber(system);
     }
 
-    private int Solve(){
+    private int Solve() throws Exception{
         this.getStatement();
 
         switch (this.sign){
             case '+':{
-                return (number1+number2);
+                return (number1 + number2);
             }
             case '-':{
-                return number1-number2;
+                return number1 - number2;
             }
             case '*':{
-                return number1*number2;
+                return number1 * number2;
             }
             case '/':{
-                return number1/number2;
+                if(number2!=0){
+                    return number1 / number2;
+                }
+                else{
+                    throw new Exception("Деление на 0");
+                }
             }
         }
         return 0;
     }
 
-    public void getResult(){
+    public void getResult () throws Exception{
         result = this.Solve();
         String resultB = Integer.toBinaryString(result);
         String resultO = Integer.toOctalString(result);
